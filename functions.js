427c7fb2aff72ee0848864d535ff2f5b48c46890
hectorcoronado <coronado.hector@gmail.com>
2016-03-16 16:47:30 -0700

    Solved for function maxOrMin()

--- functions.js
+++ functions.js
@@ -1,1 +1,24 @@
-// YOUR CODE HERE
+//1. maxOrMin(num1, num2, max)
+/*Declare function maxOrMin(numberOne, numberTwo, bool) that returns
+larger of the two numbers if bool is true, otherwise the lesser of
+the two numbers*/
+
+function maxOrMin (a, b, bool) {
+  if (bool === true) {
+    if (a > b) {
+      return a;
+    } else return b;
+  } else if (bool === false) {
+    if (a < b) {
+      return a;
+    } else return b;
+  }
+}
+
+//2. sillySum(arr)
+/*Takes array of numbers and returns sum of each number multiplied by its
+index.*/
+
+function sillySum(arr) {
+  
+}
