ed6061519d5d4ab53de0348afa775606a34e9ec6
smicl11 <bryan.smith094@gmail.com>
2016-03-16 17:41:21 -0700

    one down

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
 // YOUR CODE HERE
+
+// maxOrMin(num1, num2, max)
+function maxOrMin(num1, num2, max) {
+  if (num1 > num2 && max === true) {
+    return num1;
+  } else if (num1 < num2 && max === true) {
+    return num2;
+  }
+}
