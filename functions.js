d3504cc00cccfb52d08a7dd4fe22b54b9b011ecf
ilterates <iltates@gmail.com>
2016-03-16 19:37:31 -0700

    Exercise 3 complete

--- functions.js
+++ functions.js
@@ -23,3 +23,22 @@ for (var i = 0; myArray.length > i; i++){
 }
 
 console.log(multi);
+
+
+### `numSquare(max)`
+
+Create a function called `numSquare` that will return an array of all perfect square numbers up to, but not exceeding a max number.
+
+var myArray=[1,2,3,4];
+var square;
+function numSquare(y){
+  for (var i=0; i < myArray.length; i++){ // SYNTAX ERRORS!
+   if ( y < myArray[i]) {
+    square = myArray[i] * myArray[i];
+    console.log(square);
+    }
+    else console.log("number is bigger");
+}
+}
+
+numSquare(3);
