c2c7f607e149a47ed26a478cd4c08988b5cf9f63
Ruhsam <ruhsam@gmail.com>
2016-03-16 23:10:42 -0700

    Functions completed

--- functions.js
+++ functions.js
@@ -1,1 +1,29 @@
-// YOUR CODE HERE
+ YOUR CODE HERE
+
+sillySum
+
+var theArray = [1,2,3,4];
+var indexing = theArray.indexof
+for (i = 0; i < theArray.length; i++) {
+function sillySum(indexing += ([i] * indexing))
+theArray.forEach(sillySum(indexing += ([i] * indexing))
+}
+
+isPrime
+
+var x = Math.sqrt(10);
+console.log(x);
+var y = x - (Math.round(x));
+console.log(y);
+y===0?true:false;
+
+filter
+var array = [1, 4, 9]
+var newArray = []
+var y = Math.sqrt(i)
+var x = (y - (Math.round(y))
+
+var filter = function(arr, callback) {
+ for (i = 0; i < array.length; i++);
+    (callback(array.entries[i])?(newArray[i]): "False";
+       console.log(newArray);
