93f7ff0a8d8b1b02ffcc553c938e171d7bd45fcf
lsquach <lsquach@gmail.com>
2016-03-16 20:52:29 -0700

    solved exercise 1

--- functions.js
+++ functions.js
@@ -1,1 +1,23 @@
-// YOUR CODE HERE
+// maxOrMin(num1, num2, max)
+
+// Define a function maxOrMin that takes three parameters: two numbers and a boolean.
+// Have it return the larger of the two numbers if the boolean is true,
+// otherwise have it return the lesser of the numbers.
+
+function maxOrMin(num1, num2, max) {
+    if (max) {
+        return (num1 > num2) ? num1:num2;
+    }
+    else {
+        return (num1 > num2) ? num2:num1;
+    }
+}
+
+maxOrMin(4, 6, false);
+
+
+// sillySum(arr)
+//
+// Write a function that takes an array of numbers and returns the sum of each number multiplied by its index.
+//
+// count += (number * index)
