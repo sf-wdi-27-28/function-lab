d1fdf49fdf3ad584e5f9d2689cee7d3adef80d6d
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-16 18:19:07 -0700

    finished firstwo problems

--- functions.js
+++ functions.js
@@ -1,1 +1,22 @@
-// YOUR CODE HERE
+// maxOrMin
+function maxOrMin(num1, num2, bool) {
+  if(bool) {
+    return (num1 > num2)?num1:num2;
+  } else if (!bool) {
+    return (num1 < num2)?num1:num2;
+  }
+}
+
+maxOrMin(6, 2, false);
+
+//sillySum(arr)
+
+var randomNumbers = [1, 5, 3, 10, 9];
+
+function sillySum(array) {
+   return array.map(function newArrayFunction(currentValue, index) {
+       return currentValue + (currentValue * index);
+   });
+}
+
+sillySum(randomNumbers);
