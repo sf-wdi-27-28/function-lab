091a88a59c0e2d9a1e77a04f5fe8bf2ea490df74
ilterates <iltates@gmail.com>
2016-03-16 19:57:11 -0700

    working on exercise 5

--- functions.js
+++ functions.js
@@ -54,3 +54,27 @@ function isPrime(num){
   } else
   return false;
 }
+
+### `primes(max)`
+
+Using your `isPrime` function, create a function primes that will return an array of all prime numbers up to, but not exceeding a max number.
+
+// working on it
+var ilter = [1,2,3,4];
+var ilter2 = [];
+var ilter3=[];
+//for (i = 0; i < ilter.length; i++){}
+
+ilter.forEach(function isPrime(element){
+  if (element % element === 0){
+ilter2.push(element);
+
+
+}
+else ilter3.push(element);
+
+
+
+  });
+console.log(ilter2);
+console.log(ilter3);
