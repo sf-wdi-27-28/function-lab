5ef5b0f51d8b6533afc9a85ecdf9aef25f181d85
ilterates <iltates@gmail.com>
2016-03-16 19:14:35 -0700

    Exercise 2 complete

--- functions.js
+++ functions.js
@@ -1,1 +1,25 @@
 // YOUR CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+
+### `sillySum(arr)`
+// YOUR CODE HERE
+var myArray=[1,4,5];
+var multi = 0;
+
+
+for (var i = 0; myArray.length > i; i++){
+
+   multi += (myArray[i] * i); // I made syntax error here. because of () I spent hours! I better learn from it :)
+}
+
+console.log(multi);
